d3381ade2ab316bb26fd1bc19f2f2e472f7f09ca
Lucas Rangit Magasweran <external+github.com@magasweran.com>
2012-10-30 16:45:28 -0700

    refactoring

--- cvdemo.c
+++ cvdemo.c
@@ -4,11 +4,19 @@
 #include <highgui.h>
 #include "libfreenect_cv.h"
 
+
+#define WINDOW_RGB 		"RGB"
+#define WINDOW_DEPTH 	"Depth"
+
+static int x_offset = 0;
+static int y_offset = 0;
+
 //static IplImage *image_mask_smooth;
+
 static int x_click = -1;
 static int y_click = -1;
 
-IplImage *GlViewColor(IplImage *depth)
+static IplImage *GlViewColor(IplImage *depth)
 {
 	static IplImage *image = 0;
 	if (!image) image = cvCreateImage(cvSize(640,480), 8, 3);
@@ -60,6 +68,7 @@ IplImage *GlViewColor(IplImage *depth)
 	return image;
 }
 
+#if 0
 /*
  * From their data, a basic first order approximation for converting the raw
  * 11-bit disparity value to a depth value in centimeters is:
@@ -67,15 +76,12 @@ IplImage *GlViewColor(IplImage *depth)
  * This approximation is approximately 10 cm off at 4 m away, and less than
  * 2 cm off within 2.5 m.
  */
-float raw_depth_to_meters_0(int raw_disparity)
+float raw_depth_to_meters(int raw_disparity)
 {
-	if (raw_disparity < 2047)
-	{
-		return 1.0 / (raw_disparity * -0.0030711016 + 3.3309495161);
-	}
-	return 0;
+	float depth = 1.0 / (raw_disparity * -0.0030711016 + 3.3309495161);
+	return depth;
 }
-
+#else
 /*
  * A better approximation given by Stéphane Magnenat:
  *   distance = 0.1236 * tan(rawDisparity / 2842.5 + 1.1863) in meters.
@@ -84,18 +90,19 @@ float raw_depth_to_meters_0(int raw_disparity)
  * 1/x approximation is about 1.7 cm.
  *
  */
-float raw_depth_to_meters(int raw_disparity)
+static float raw_depth_to_meters(int raw_disparity)
 {
 	float depth = 0.1236 * tanf(raw_disparity/2842.5 + 1.1863);
 	return depth;
 }
+#endif
 
-enum {BLUE_INDEX=0, GREEN_INDEX=1, RED_INDEX=3};
+enum {BGR_BLUE_INDEX=0, BGR_GREEN_INDEX=1, BGR_RED_INDEX=3};
 /*
  * Color only value for disparity values in the ranges of interest.
- * Specifically, keep only the "normal" range and in the "no data" range.
+ * Specifically, keep only the "no data" range.
  */
-IplImage *kinect_disparity_filter(IplImage *depth)
+static IplImage *kinect_disparity_filter(IplImage *depth)
 {
 	static IplImage *image = 0;
 	if (!image) image = cvCreateImage( cvSize(depth->width, depth->height), IPL_DEPTH_8U, 3); // depth->depth Unsigned 8-bit integer (8u)
@@ -107,37 +114,37 @@ IplImage *kinect_disparity_filter(IplImage *depth)
 		if (raw_disparity < 242) // (depth_meters < 0.4)
 		{
 			// unknown
-			depth_color[3*i+RED_INDEX]	= 0;
-			depth_color[3*i+GREEN_INDEX]	= 0;
-			depth_color[3*i+BLUE_INDEX]	= 0;
+			depth_color[3*i+BGR_RED_INDEX]	= 0;
+			depth_color[3*i+BGR_GREEN_INDEX]	= 0;
+			depth_color[3*i+BGR_BLUE_INDEX]	= 0;
 		}
 		else if (raw_disparity < 658) // (depth_meters < 0.8)
 		{
 			// too close
-			depth_color[3*i+RED_INDEX]	= 0;
-			depth_color[3*i+GREEN_INDEX]	= 0;
-			depth_color[3*i+BLUE_INDEX]	= 0;
+			depth_color[3*i+BGR_RED_INDEX]	= 0;
+			depth_color[3*i+BGR_GREEN_INDEX]	= 0;
+			depth_color[3*i+BGR_BLUE_INDEX]	= 0;
 		}
 		else if (raw_disparity < 1006) // (depth_meters < 4.0)
 		{
 			// normal
-			depth_color[3*i+RED_INDEX]	= 0;
-			depth_color[3*i+GREEN_INDEX]	= 0;
-			depth_color[3*i+BLUE_INDEX]	= 0;
+			depth_color[3*i+BGR_RED_INDEX]	= 0;
+			depth_color[3*i+BGR_GREEN_INDEX]	= 0;
+			depth_color[3*i+BGR_BLUE_INDEX]	= 0;
 		}
 		else if (raw_disparity < 1050) // (depth_meters < 8.0)
 		{
 			// too far
-			depth_color[3*i+RED_INDEX]	= 0;
-			depth_color[3*i+GREEN_INDEX]	= 0;
-			depth_color[3*i+BLUE_INDEX]	= 0;
+			depth_color[3*i+BGR_RED_INDEX]	= 0;
+			depth_color[3*i+BGR_GREEN_INDEX]	= 0;
+			depth_color[3*i+BGR_BLUE_INDEX]	= 0;
 		}
 		else  // if (depth_meters > 8.0)
 		{
 			// unknown
-			depth_color[3*i+RED_INDEX]	= 0;
-			depth_color[3*i+GREEN_INDEX]	= 255;
-			depth_color[3*i+BLUE_INDEX]	= 0;
+			depth_color[3*i+BGR_RED_INDEX]	= 0;
+			depth_color[3*i+BGR_GREEN_INDEX]	= 255;
+			depth_color[3*i+BGR_BLUE_INDEX]	= 0;
 		}
 	}
 	return image;
@@ -177,7 +184,7 @@ void mouseHandler(int event, int x, int y, int flags, void *param)
 	}
 }
 
-void get_cv_info() {
+static void get_cv_info() {
 	const char* libraries;
 	const char* modules;
 	// Using cvGetModuleInfo() to check for IPP
@@ -185,7 +192,7 @@ void get_cv_info() {
 	printf("Libraries: %s\nModules: %s\n", libraries, modules);
 }
 
-IplImage* detect_contours(IplImage* img)
+static IplImage* detect_contours(IplImage* img)
 {
 	CvSeq* contours;
 	CvSeq* result;
@@ -222,12 +229,6 @@ IplImage* detect_contours(IplImage* img)
 
 	return ret;
 }
-
-#define WINDOW_RGB "RGB"
-#define WINDOW_DEPTH "Depth"
-
-static int x_offset = 0;
-static int y_offset = 0;
 
 int main(int argc, char **argv)
 {
