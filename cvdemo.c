3e4ab88033668f512850956e8d92b5035d7944bb
Lucas Rangit Magasweran <external+github.com@magasweran.com>
2012-11-05 09:24:22 -0800

    * improve depth filtering.  *arrow keys shift depth image so that it can be aligned manually with RGB image (for now).  * add a sample camera test function for Michael.

--- cvdemo.c
+++ cvdemo.c
@@ -1,72 +1,19 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 #include <cv.h>
 #include <highgui.h>
 #include "libfreenect_cv.h"
 
-
+#define PROCESS_FPS		2
 #define WINDOW_RGB 		"RGB"
 #define WINDOW_DEPTH 	"Depth"
 
-static int x_offset = 0;
-static int y_offset = 0;
-
-//static IplImage *image_mask_smooth;
-
 static int x_click = -1;
 static int y_click = -1;
-
-static IplImage *GlViewColor(IplImage *depth)
-{
-	static IplImage *image = 0;
-	if (!image) image = cvCreateImage(cvSize(640,480), 8, 3);
-	unsigned char *depth_mid = (unsigned char*)(image->imageData);
-	int i;
-	for (i = 0; i < 640*480; i++) {
-		int lb = ((short *)depth->imageData)[i];
-		lb %= 256;
-		int ub = ((short *)depth->imageData)[i];
-		ub /= 256;
-		switch (ub) {
-			case 0:
-				depth_mid[3*i+2] = 255;
-				depth_mid[3*i+1] = 255-lb;
-				depth_mid[3*i+0] = 255-lb;
-				break;
-			case 1:
-				depth_mid[3*i+2] = 255;
-				depth_mid[3*i+1] = lb;
-				depth_mid[3*i+0] = 0;
-				break;
-			case 2:
-				depth_mid[3*i+2] = 255-lb;
-				depth_mid[3*i+1] = 255;
-				depth_mid[3*i+0] = 0;
-				break;
-			case 3:
-				depth_mid[3*i+2] = 0;
-				depth_mid[3*i+1] = 255;
-				depth_mid[3*i+0] = lb;
-				break;
-			case 4:
-				depth_mid[3*i+2] = 0;
-				depth_mid[3*i+1] = 255-lb;
-				depth_mid[3*i+0] = 255;
-				break;
-			case 5:
-				depth_mid[3*i+2] = 0;
-				depth_mid[3*i+1] = 0;
-				depth_mid[3*i+0] = 255-lb;
-				break;
-			default:
-				depth_mid[3*i+2] = 0;
-				depth_mid[3*i+1] = 0;
-				depth_mid[3*i+0] = 0;
-				break;
-		}
-	}
-	return image;
-}
+static int canny_low = 80;
+static int canny_high = 100;
+static bool clear = 0;
 
 #if 0
 /*
@@ -101,6 +48,7 @@ enum {BGR_BLUE_INDEX=0, BGR_GREEN_INDEX=1, BGR_RED_INDEX=3};
 /*
  * Color only value for disparity values in the ranges of interest.
  * Specifically, keep only the "no data" range.
+ * @todo remove right no data band
  */
 static IplImage *kinect_disparity_filter(IplImage *depth)
 {
@@ -158,14 +106,16 @@ void mouseHandler(int event, int x, int y, int flags, void *param)
 		/* left button down */
 		x_click = x;
 		y_click = y;
-		fprintf(stdout, "Left button down (%d, %d).\n", x, y);
+//		fprintf(stdout, "Left button down (%d, %d).\n", x, y);
 		break;
-
+	case CV_EVENT_LBUTTONDBLCLK:
+		break;
 	case CV_EVENT_RBUTTONDOWN:
 		/* right button down */
-		fprintf(stdout, "Right button down (%d, %d).\n", x, y);
+//		fprintf(stdout, "Right button down (%d, %d).\n", x, y);
 		break;
-
+	case CV_EVENT_RBUTTONDBLCLK:
+		break;
 	case CV_EVENT_MOUSEMOVE:
 		/* mouse move */
 #if 0
@@ -192,6 +142,9 @@ static void get_cv_info() {
 	printf("Libraries: %s\nModules: %s\n", libraries, modules);
 }
 
+/*
+ * Find contours of ceiling lights.
+ */
 static IplImage* detect_contours(IplImage* img)
 {
 	CvSeq* contours;
@@ -199,27 +152,29 @@ static IplImage* detect_contours(IplImage* img)
 	CvMemStorage *storage = cvCreateMemStorage(0);
 	static IplImage* ret = NULL;
 	if (!ret) ret = cvCreateImage(cvGetSize(img), 8, 1);
+	cvZero(ret);
 	IplImage* temp = cvCreateImage(cvGetSize(img), 8, 1);
 	int i;
 	double area;
 	cvCopy(img, temp, NULL);
 	cvFindContours(temp, storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));
-	while(contours)
+	while (contours)
 	{
 		result = cvApproxPoly(contours, sizeof(CvContour), storage, CV_POLY_APPROX_DP, cvContourPerimeter(contours)*0.02, 0);
 		area = fabs(cvContourArea(result, CV_WHOLE_SEQ, 0));
-		if ((result->total==4)  &&
-			(area > 1000) && // @TODO determine area for smallest light and largest
-			(cvCheckContourConvexity(result)))
+		if ((4 == result->total)  && // has 4 vertices
+			(area > 60000) && // @TODO determine area for smallest light and largest
+			(cvCheckContourConvexity(result))) // is convex
 		{
 			CvPoint *pt[4];
 			for( i=0; i<4; i++)
 				pt[i] = (CvPoint*)cvGetSeqElem(result, i);
-
+			// draw contour
 			cvLine(ret, *pt[0], *pt[1], cvScalarAll(255), 1, 8, 0);
 			cvLine(ret, *pt[1], *pt[2], cvScalarAll(255), 1, 8, 0);
 			cvLine(ret, *pt[2], *pt[3], cvScalarAll(255), 1, 8, 0);
 			cvLine(ret, *pt[3], *pt[0], cvScalarAll(255), 1, 8, 0);
+			fprintf(stdout, "(%d,%d) (%d,%d) (%d,%d) (%d,%d)\n", pt[0]->x, pt[0]->y, pt[1]->x, pt[1]->y, pt[2]->x, pt[2]->y, pt[3]->x, pt[3]->y);
 		}
 		contours = contours->h_next;
 	}
@@ -230,46 +185,100 @@ static IplImage* detect_contours(IplImage* img)
 	return ret;
 }
 
+/*
+ * Handle Key Input
+ * Note, after making certain changes (such as shifting) any images that
+ * are comprised of multiple samples must be cleared.
+ */
+static void adjust_offset(char key, int *x_offset, int *y_offset) {
+	switch (key)
+	{
+	case -1:
+	case 'q':
+		break;
+	case 81: // left
+		*x_offset -= 1;
+		clear = true;
+		break;
+	case 82: // up
+		*y_offset -= 1;
+		clear = true;
+		break;
+	case 83: // right
+		*x_offset += 1;
+		clear = true;
+		break;
+	case 84: // down
+		*y_offset += 1;
+		clear = true;
+		break;
+	case '-': // scale down
+		// @todo
+		clear = true;
+		break;
+	case '=': // scale up
+		// @todo
+		clear = true;
+		break;
+	case 'c':
+		// clear
+		clear = true;
+		break;
+	default:
+		break;
+	}
+}
+
+static int camera_test()
+{
+   CvCapture* capture = cvCaptureFromCAM( CV_CAP_ANY );
+   if ( !capture ) {
+     fprintf( stderr, "ERROR: capture is NULL \n" );
+     getchar();
+     return -1;
+   }
+   // Create a window in which the captured images will be presented
+   cvNamedWindow( "mywindow", CV_WINDOW_AUTOSIZE );
+   // Show the image captured from the camera in the window and repeat
+   while ( 1 ) {
+     // Get one frame
+     IplImage* frame = cvQueryFrame( capture );
+     if ( !frame ) {
+       fprintf( stderr, "ERROR: frame is null...\n" );
+       getchar();
+       break;
+     }
+     cvShowImage( "mywindow", frame );
+     // Do not release the frame!
+     //If ESC key pressed, Key=0x10001B under OpenCV 0.9.7(linux version),
+     //remove higher bits using AND operator
+     if ( (cvWaitKey(10) & 255) == 27 ) break;
+   }
+   // Release the capture device housekeeping
+   cvReleaseCapture( &capture );
+   cvDestroyWindow( "mywindow" );
+   return 0;
+ }
+
 int main(int argc, char **argv)
 {
-	CvMemStorage* storage = cvCreateMemStorage(0);
 	CvFont font;
 	char key;
+	int x_offset = 0;
+	int y_offset = 0;
+	IplImage *image_mask_smooth = cvCreateImage( cvSize(640, 480), IPL_DEPTH_8U, 1);
+	cvZero(image_mask_smooth);
+
 	cvInitFont(&font, CV_FONT_HERSHEY_SIMPLEX, 1.0, 1.0, 0, 1, CV_AA);
 	cvNamedWindow( WINDOW_RGB, CV_WINDOW_AUTOSIZE);
 	cvSetMouseCallback( WINDOW_RGB, mouseHandler, NULL );
 
+	camera_test();
 //	get_cv_info();
 
+	// process frames indefinitely until user presses 'q' for quit
 	while (key != 'q')
 	{
-		switch (key)
-		{
-		case -1:
-		case 'q':
-			break;
-		case 81:	// left
-			x_offset--;
-			break;
-		case 82:	// up
-			y_offset--;
-			break;
-		case 83:	// right
-			x_offset++;
-			break;
-		case 84:	// down
-			y_offset++;
-			break;
-		case '-':	// scale down
-			// @todo
-			break;
-		case '=':	// scale up
-			// @todo
-			break;
-		default:
-			break;
-		}
-
 		IplImage *image_rgb = freenect_sync_get_rgb_cv(0);
 		if (!image_rgb) {
 		    printf("Error: Kinect not connected?\n");
@@ -282,7 +291,6 @@ int main(int argc, char **argv)
 		    printf("Error: Kinect not connected?\n");
 		    return -1;
 		}
-
 		IplImage *image_depth = kinect_disparity_filter(image_disparity);
 
 		// shift depth image
@@ -312,75 +320,72 @@ int main(int argc, char **argv)
 		cvResetImageROI(image_depth);
 		image_depth = image_depth_shifted;
 
+		// blend RGB and disparity frames after shift
 		IplImage* image_blended = cvCreateImage( cvGetSize(image_depth), IPL_DEPTH_8U, 3);
 		cvAddWeighted(image_rgb, 1.0, image_depth, 1.0, 0.0, image_blended);
 		cvShowImage( "Blended", image_blended);
 
-
+		// create binary image of disparity
 		IplImage *image_depth_gray = cvCreateImage( cvGetSize(image_depth), IPL_DEPTH_8U, 1);
 		cvCvtColor( image_depth, image_depth_gray, CV_RGB2GRAY);
-//		cvShowImage( "Depth Gray", image_depth_gray);
 		IplImage *image_mask = cvCreateImage( cvGetSize(image_depth_gray), IPL_DEPTH_8U, 1);
 		cvThreshold(image_depth_gray, image_mask, 128, 255, CV_THRESH_BINARY);
 
-#if 0 	// can't take multiple samples if the depth data can be moved around
-		// take multiple samples of the mask
-		if (NULL == image_mask_smooth)
+		// take multiple samples of the depth
+		// this fills in the depth mask
+		if (clear)
 		{
-			image_mask_smooth = cvCreateImage( cvGetSize(image_mask), IPL_DEPTH_8U, 1);
-			cvCopy( image_mask, image_mask_smooth, NULL);
+			clear = false;
+			cvZero(image_mask_smooth);
 		}
-		else
-		{
-			cvAdd( image_mask, image_mask_smooth, image_mask_smooth, NULL);
-			//cvAnd( image_mask, image_mask_smooth, image_mask_smooth, NULL);
-		}
-		//		cvShowImage( "Mask And", image_mask_smooth);
-		image_mask = image_mask_smooth;
-		cvShowImage( "Mask", image_mask);
+		cvAdd( image_mask, image_mask_smooth, image_mask_smooth, NULL);
+//		cvAnd( image_mask, image_mask_smooth, image_mask_smooth, NULL);
+		cvShowImage("Mask Smooth", image_mask_smooth);
+
+#if 0
+		IplImage *image_rgb_masked = cvCreateImage(cvGetSize(image_rgb), IPL_DEPTH_8U, 3);
+		cvCopy(image_rgb, image_rgb_masked, image_mask);
+		cvShowImage("RGB Mask", image_rgb_masked);
 #endif
-		IplImage *image_rgb_masked = cvCreateImage( cvGetSize(image_rgb), IPL_DEPTH_8U, 3);
-		cvCopy( image_rgb, image_rgb_masked, image_mask);
-		cvShowImage( "RGB Mask", image_rgb_masked);
 
-		IplImage *image_contours = detect_contours(image_mask);
-	    cvShowImage( "Contours", image_contours);
+		// find polygons in the disparity data
+		IplImage* disparity_contours = cvCreateImage( cvGetSize(image_mask_smooth), IPL_DEPTH_8U, 1);
+		cvCopy( detect_contours(image_mask_smooth), disparity_contours, NULL);
+		cvShowImage("Disparity Contours", disparity_contours);
 
+		// find polygons in the RGB data
 		cvSmooth(image_rgb, image_rgb, CV_GAUSSIAN, 5, 5, 0, 0);
-		IplImage* image_gray = cvCreateImage( cvGetSize(image_rgb), IPL_DEPTH_8U, 1);
-		cvCvtColor( image_rgb, image_gray, CV_RGB2GRAY);
-		IplImage* image_edges = cvCreateImage( cvGetSize(image_gray), IPL_DEPTH_8U, 1);
-		cvCanny( image_gray, image_edges, 10, 100, 3 );
-		cvShowImage( "Edges", image_edges);
+		IplImage* image_gray = cvCreateImage(cvGetSize(image_rgb), IPL_DEPTH_8U, 1);
+		cvCvtColor(image_rgb, image_gray, CV_RGB2GRAY);
 
-		CvSeq* results = cvHoughLines2(
-				image_edges,
-				storage,
-				CV_HOUGH_STANDARD,
-				2.0, 2.0,
-				image_edges->width/10,
-				0, 0
-		);
+		IplImage* image_edges = cvCreateImage(cvGetSize(image_gray), IPL_DEPTH_8U, 1);
+		cvCanny(image_gray, image_edges, canny_low, canny_high, 3);
+		cvSmooth(image_edges, image_edges, CV_GAUSSIAN, 5, 5, 0, 0);
 
+		cvShowImage("Edges", image_edges);
+		IplImage* rgb_contours = cvCreateImage(cvGetSize(image_edges), IPL_DEPTH_8U, 1);
+		cvCopy( detect_contours(image_edges), rgb_contours, NULL);
+		cvShowImage("RGB Contours", rgb_contours);
+
+//		CvSeq* results = cvHoughLines2(image_edges, storage, CV_HOUGH_STANDARD, 2.0, 2.0, image_edges->width / 10, 0, 0);
 
 		if (-1 != x_click && -1 != y_click)
 		{
 			char coord_str[] = "640,480,-01.234";
 			int coord_str_len = strlen(coord_str);
-			int pixel_disparity = ((short *)image_disparity->imageData)[y_click * 640 + x_click];
+			int pixel_disparity = ((short *) image_disparity->imageData)[y_click * 640 + x_click];
 			sprintf(coord_str, "%03d,%03d,%04d", x_click, y_click, pixel_disparity);
 //			float pixel_depth_meters = raw_depth_to_meters(pixel_disparity);
 //			sprintf(coord_str, "%03d,%03d,%02.03f", x_click, y_click, pixel_depth_meters);
 			coord_str[coord_str_len] = '\0';
 			cvPutText(image_rgb, coord_str, cvPoint(x_click, y_click), &font, cvScalar(255, 255, 255, 0));
 		}
-
 		cvShowImage(WINDOW_RGB, image_rgb);
 //		cvShowImage(WINDOW_DEPTH, GlViewColor(depth));
 		cvShowImage(WINDOW_DEPTH, image_depth);
-		//cvShowImage(WINDOW_DEPTH, depth);
-
-		key = cvWaitKey(500);
+//		cvShowImage(WINDOW_DEPTH, depth);
+		key = cvWaitKey(1000/PROCESS_FPS);
+		adjust_offset(key, &x_offset, &y_offset);
 	}
 
 	return 0;
